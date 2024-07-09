a294e497f50431c9eb1c0a930a0ec77aa5fb7fb5
David Salazar <davidsp302@gmail.com>
2024-07-09 11:52:31 -0500

    se agrega modal

---
+++ Modal/app.js
@@ -0,0 +1,7 @@
+document.getElementById('open-modal').addEventListener('click', ()=>{
+    document.getElementById('modal').classList = 'modal'
+});
+
+document.getElementById('close').addEventListener('click', ()=>{
+    document.getElementById('modal').classList = 'modal-hide'
+});
