0180c9d478994c15011e7a5a28120cfcc387d558
David Salazar <davidsp302@gmail.com>
2024-07-12 11:53:56 -0500

    se agrega video v2

--- VideoProject/app.js
+++ VideoProject/app.js
@@ -5,12 +5,17 @@ boton.addEventListener("click", ()=>{
     if(video.paused){
         video.play();
         document.getElementById('sw').className = 'sw';
-        document.querySelector('i').className= 'fa-solid fa-pause';
+        document.getElementById('icon').className= 'fa-solid fa-pause';
         document.getElementById('labelPlay').style.color = 'rgb(27, 119, 161)';
     }else{
         video.pause();
         document.getElementById('sw').className = 'sw-p';
-        document.querySelector('i').className= 'fa-solid fa-play';
+        document.getElementById('icon').className= 'fa-solid fa-play';
         document.getElementById('labelPause').style.color = 'red';
     }
+});
+
+document.getElementById('random').addEventListener('click', ()=>{
+    let numRandom = Math.floor(Math.random() * 7) + 1;
+    document.getElementById('video').src = `/VideoProject/asset/${numRandom}.mp4`;
 });
